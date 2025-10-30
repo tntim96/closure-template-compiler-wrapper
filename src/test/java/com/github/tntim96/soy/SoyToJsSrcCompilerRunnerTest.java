02d36548b5fc09b4faafd9664881dc5413d52f9b
tntim96 <tntim96@gmail.com>
2025-10-30 15:24:51 +1100

    Upgrade Soy 2017-08-08 to 2018-01-03 Bump POM version from 2017-08-08-02 to 2018-01-03-01-SNAPSHOT

--- src/test/java/com/github/tntim96/soy/SoyToJsSrcCompilerRunnerTest.java
+++ src/test/java/com/github/tntim96/soy/SoyToJsSrcCompilerRunnerTest.java
@@ -15,7 +15,7 @@ public class SoyToJsSrcCompilerRunnerTest {
       fail("Expected an exception to be thrown, but none was thrown.");
     } catch (Exception e) {
       System.out.println("e = " + e);
-      assertEquals("com.google.template.soy.AbstractSoyCompiler$CommandLineError", e.getCause().getCause().getClass().getName());
+      assertEquals("com.google.template.soy.CommandLineError", e.getCause().getCause().getClass().getName());
     }
   }
 }
