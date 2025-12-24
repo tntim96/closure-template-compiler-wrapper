2afce952ee8a79c72ec5d6c7bc88d23e5539fd49
tntim96 <tntim96@gmail.com>
2025-12-24 17:56:43 +1100

    Bump POM version to "2018-03-14-02-SNAPSHOT" and add support for SoyToIncrementalDomSrcCompiler

---
+++ src/test/java/com/github/tntim96/soy/SoyToIncrementalDomSrcCompilerRunnerTest.java
@@ -0,0 +1,21 @@
+package com.github.tntim96.soy;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+public class SoyToIncrementalDomSrcCompilerRunnerTest {
+
+  @Test
+  public void shouldNotExitJvm() {
+    try {
+      SoyToJsSrcCompilerRunner.main(new String[]{});
+      fail("Expected an exception to be thrown, but none was thrown.");
+    } catch (Exception e) {
+      System.out.println("e = " + e);
+      assertEquals("com.google.template.soy.CommandLineError", e.getCause().getCause().getClass().getName());
+    }
+  }
+}
